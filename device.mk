6781af43905db834c1b5318950e4df048e638751
chandu078 <chandudyavanapelli03@gmail.com>
2024-02-22 20:31:06 +0530

    opluscamera: Add support for oplus camera
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: bijoyv9 <bijoyv9@gmail.com>

--- device.mk
+++ device.mk
@@ -3,5 +3,31 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from the proprietary version
+# Framework
+PRODUCT_BOOT_JARS += \
+    oplus-support-wrapper
+
+# Init
+PRODUCT_PACKAGES += \
+    init.oplus.camera.rc
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/oplus_google_lens_config.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/oplus_google_lens_config.xml \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-oplus.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-oplus.xml \
+    $(LOCAL_PATH)/configs/sysconfig/hiddenapi-package-oplus-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-oplus-whitelist.xml
+
+# Properties
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.vendor.camera.privapp.list=com.oplus.camera \
+    ro.com.google.lens.oem_camera_package=com.oplus.camera \
+    ro.com.google.lens.oem_image_package=com.oneplus.gallery
+
+# Photo
+TARGET_CAMERA_PACKAGE_NAME := com.oplus.camera
+
+# Video
+TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
+
+# Inherit from camera-vendor.mk
 $(call inherit-product, vendor/oplus/camera/camera-vendor.mk)
