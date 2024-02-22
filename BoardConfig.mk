e71368d8c06ff56120bffe4eab38b876008dda10
pjgowtham <pjgowtham@gmail.com>
2024-02-22 18:24:02 +0530

    opluscamera: Add sepolicy for Oplus Camera
    Co-authored-by: Mashopy <eliasgheeraert@gmail.com>
    Co-authored-by: Sugakesshaa <sugakesshaa@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,5 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# SEpolicy
+include device/oplus/camera/sepolicy/SEPolicy.mk
+
 # Inherit from the proprietary version
 include vendor/oplus/camera/BoardConfigVendor.mk
