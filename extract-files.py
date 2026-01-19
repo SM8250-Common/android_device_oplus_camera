97d0daf16d39ddd9432545b5adb501d5f0d67900
bijoyv9 <bijoyv9@gmail.com>
2026-01-19 06:59:18 +0000

    opluscamera: Extract from CPH2581_14.0.0.850(EX01)

--- extract-files.py
+++ extract-files.py
@@ -4,10 +4,27 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+
+def lib_fixup_system_ext_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'system_ext' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'libSuperTextWrapper',
+        'libXDocProcessSDK',
+        'libYTCommon',
+        'libmpbase',
+    ): lib_fixup_system_ext_suffix,
+}
 
 namespace_imports = [
     'device/oplus/camera',
@@ -16,6 +33,7 @@ namespace_imports = [
 module = ExtractUtilsModule(
     'camera',
     'oplus',
+    lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
 )
 
